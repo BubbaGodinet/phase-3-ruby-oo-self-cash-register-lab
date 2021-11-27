955c77c9c428d715dfb0c09a3706d9522d14e5ea
BubbaGodinet <godinetvn@gmail.com>
2021-11-26 17:58:25 -0700

    commit tree 18a177e68e502b23d359f88c3152be05ffe28dd7

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,17 @@
+require 'pry'
+class CashRegister
 
+    def initialize(total)
+        @total = 0
+        @employee_discount = nil
+    end
+
+    def total
+        @total
+    end
+end
+
+
+binding.pry
+
+0
